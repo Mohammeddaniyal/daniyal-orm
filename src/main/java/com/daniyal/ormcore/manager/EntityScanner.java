9827e58c4980724c43e43570ec27f8038db5364a
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-20 14:44:01 +0530

    imported pojo

--- src/main/java/com/daniyal/ormcore/manager/EntityScanner.java
+++ src/main/java/com/daniyal/ormcore/manager/EntityScanner.java
@@ -1,5 +1,6 @@
 package com.daniyal.ormcore.manager;
 import java.lang.reflect.*;
+import com.daniyal.ormcore.pojo.*;
 class EntityScanner
 {
 public static Map<String,EntityMeta> scanBasePackage(String basePackage)
