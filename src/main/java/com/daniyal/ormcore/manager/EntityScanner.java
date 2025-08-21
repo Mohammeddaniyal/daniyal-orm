08a13da29a11afc69c439615ac20321b1052b120
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-21 20:20:48 +0530

    Feat implemented scanning class type files from either folder or jar respect to the base package

--- src/main/java/com/daniyal/ormcore/manager/EntityScanner.java
+++ src/main/java/com/daniyal/ormcore/manager/EntityScanner.java
@@ -4,8 +4,8 @@ import com.daniyal.ormcore.exceptions.*;
 import java.lang.reflect.*;
 import java.util.*;
 import java.io.*;
-import java.net.URL;
-
+import java.net.*;
+import java.util.jar.*;
 class EntityScanner
 {
 public static Map<String,EntityMeta> scanBasePackage(String basePackage) throws ORMException
@@ -13,41 +13,67 @@ public static Map<String,EntityMeta> scanBasePackage(String basePackage) throws 
 Map<String,EntityMeta> entitiesMetaMap=new HashMap<>();
 ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
 System.out.println("Base package "+basePackage);
-URL resource=classLoader.getResource(basePackage);
-if(resource==null)
-{
-throw new ORMException("Invalid base-package : "+basePackage);
-}
-String absolutePath=resource.getPath();
-if(absolutePath==null)
-{
-throw new ORMException("Invalid base-package : "+basePackage);
-}
-absolutePath=absolutePath.substring(1);
+
 try
 {
-File rootFolder=new File(absolutePath);
-if(!rootFolder.exists() || !root.isDirectory())
+String path=basePackage.replace(".",File.separator);
+System.out.println("Path :"+path);
+// Get all resources(directory or jar) for basePackage
+Enumeration<URL> resources=classLoader.getResources(path);
+URL resource;
+
+// Loop over every resource(can either be folder or jar)
+while(resources.hasMoreElements())
 {
-throw new ORMException("Invalid base-package : "+basePackage);
-}
-loadMeta(rootFolder);
-// now we have the absolutePath of the base package
-// now we'll traverse on all directories and it's sub folder
-// and identity each file and analyze classes using reflectionApi
-// and classes with @Entity tag will become the part of datastructure
-}catch(IOException ioException)
+resource=resources.nextElement();
+System.out.println("Found resource : "+resource);
+
+// if it's a folder
+if(resource.getProtocol().equals("file"))
 {
-throw new ORMException(ioExeption.getMessage());
-}
-
-private void loadMeta(File rootFolder)
+File directory=new File(resource.toURI());
+for(File file:directory.listFiles())
 {
-
+if(file.getName().endsWith(".class"))
+{
+String className=basePackage+"."+file.getName().replace(".class","");
+System.out.println("Discovered class : "+className);	
 }
+}// for loop ends on directory files list
+}// folder on disk condition ends
+else if(resource.getProtocol().equals("jar"))
+{
+// this how path looks 
+//jar:file:/C:/libs/myLib.jar!/com/example/MyClass.class
+// because of this below line it will become 
+// C:/libs/myLib.jar
+String jarPath=resource.getPath().substring("file:/".length(),resource.getPath().indexOf("!"));
+JarFile jarFile=new JarFile(jarPath);
 
+// going through every entry in the jar file
+for(JarEntry entry:java.util.Collections.list(jarFile.entries()))
+{
+String name=entry.getName();
 
+// only picking .class files inside our package 
+if(name.startsWith(path) && name.endsWith(".class"))
+{
+String className=name.replace("/",".").replace(".class","");
+System.out.println("Discovered class : "+className);	
+}
+} // for loop on jar entries ends here
+
+}// jar part ends here
+
+}// loop ends on resources
+}catch(IOException | URISyntaxException exception)
+{
+throw new ORMException(exception.getMessage());
+}
 return entitiesMetaMap;
-}
+
+}// function ends
+
+
 }
 
