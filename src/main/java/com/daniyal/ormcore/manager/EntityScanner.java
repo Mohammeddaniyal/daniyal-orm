a00c04a8926f0772f013283ad30c53a6c1133f1a
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-21 14:03:31 +0530

    Fixed

--- src/main/java/com/daniyal/ormcore/manager/EntityScanner.java
+++ src/main/java/com/daniyal/ormcore/manager/EntityScanner.java
@@ -1,27 +1,26 @@
 package com.daniyal.ormcore.manager;
+import com.daniyal.ormcore.pojo.*;
+import com.daniyal.ormcore.exceptions.*;
 import java.lang.reflect.*;
 import java.util.*;
-import com.daniyal.ormcore.pojo.*;
 import java.io.*;
 import java.net.URL;
+
 class EntityScanner
 {
 public static Map<String,EntityMeta> scanBasePackage(String basePackage) throws ORMException
 {
-try
-{
 Map<String,EntityMeta> entitiesMetaMap=new HashMap<>();
 ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
+System.out.println("Base package "+basePackage);
 URL resource=classLoader.getResource(basePackage);
 if(resource==null)
 {
 throw new ORMException("Invalid base-package : "+basePackage);
 }
-String fullPath=resource.getPath();
+String fullPath=resource.getPath().substring(1);
 System.out.println("Full path : " +fullPath);
 return entitiesMetaMap;
-}catch(IOException ioException)
-{
 }
 }
-}
+
