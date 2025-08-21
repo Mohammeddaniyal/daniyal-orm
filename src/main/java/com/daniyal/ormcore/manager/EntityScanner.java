362b82e0733e953c55b130440ceb3a4beb92a2cb
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-21 12:49:44 +0530

    Added getting resource full path

--- src/main/java/com/daniyal/ormcore/manager/EntityScanner.java
+++ src/main/java/com/daniyal/ormcore/manager/EntityScanner.java
@@ -2,11 +2,26 @@ package com.daniyal.ormcore.manager;
 import java.lang.reflect.*;
 import java.util.*;
 import com.daniyal.ormcore.pojo.*;
+import java.io.*;
+import java.net.URL;
 class EntityScanner
 {
-public static Map<String,EntityMeta> scanBasePackage(String basePackage)
+public static Map<String,EntityMeta> scanBasePackage(String basePackage) throws ORMException
+{
+try
 {
 Map<String,EntityMeta> entitiesMetaMap=new HashMap<>();
+ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
+URL resource=classLoader.getResource(basePackage);
+if(resource==null)
+{
+throw new ORMException("Invalid base-package : "+basePackage);
+}
+String fullPath=resource.getPath();
+System.out.println("Full path : " +fullPath);
 return entitiesMetaMap;
+}catch(IOException ioException)
+{
+}
 }
 }
