c8c78b68ee90019e937949aefe07167158e572b2
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-21 21:28:30 +0530

    Created Utility for converting snake to camel case and vice versa

---
+++ src/main/java/com/daniyal/ormcore/utils/CaseConvertor.java
@@ -0,0 +1,54 @@
+package com.daniyal.ormcore.utils;
+public class CaseConvertor
+{
+public static String toSnakeCase(String camelCase)
+{
+if(camelCase==null || camelCase.isEmpty()) return camelCase;
+camelCase=camelCase.trim();
+StringBuilder snakeCaseBuilder=new StringBuilder();
+snakeCaseBuilder.append(camelCase.charAt(0).toLowerCase());
+char c;
+for(int i=1;i<camelCase.length();++i)
+{
+c=camelCase.charAt(i);
+if(Character.isUpperCase(c))
+{
+snakeCaseBuilder.append("_");
+snakeCaseBuilder.append(Character.toLowerCase(c));
+}
+else
+{
+snakeCaseBuilder.append(c);
+}
+}
+return snakeCaseBuilder.toString();
+}
+public static String toCamelCase(String snakeCase)
+{
+if(snakeCase==null || snakeCase.isEmpty()) return snakeCase;
+snakeCase=snakeCase.trim();
+String camelCaseBuilder=new StringBuilder();
+boolean isNextCharCapital=false;
+char c;
+for(int i=0;i<snakeCase.length();++i)
+{
+c=snakeCase.charAt(i);
+if(c=="_")
+{
+isNextCharCapital=true;
+}
+else
+{
+if(isNextCharCapital)
+{
+camelCaseBuilder.append(Character.toUpperCase(c));
+}
+else
+{
+camelCaseBuilder.append(c);
+}
+}
+} // loop ends
+return camelCaseBuilder.toString()
+} toCamelCase
+}// class ends
