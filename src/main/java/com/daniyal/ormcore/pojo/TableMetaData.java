b549748227836780d1d2036d6f7e0934bd4dc3d0
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-22 11:42:37 +0530

    represents a table

---
+++ src/main/java/com/daniyal/ormcore/pojo/TableMetaData.java
@@ -0,0 +1,29 @@
+package com.daniyal.ormcore.pojo;
+import java.util.*;
+public class TableMetaData
+{
+private String tableName;
+private List<ColumnMetaData> columns;
+public TableMetaData()
+{
+this.tableName="";
+this.columns=null;
+}
+public void setTableName(java.lang.String tableName)
+{
+this.tableName=tableName;
+}
+public java.lang.String getTableName()
+{
+return this.tableName;
+}
+public void setColumns(java.util.List columns)
+{
+this.columns=columns;
+}
+public java.util.List getColumns()
+{
+return this.columns;
+}
+
+}
