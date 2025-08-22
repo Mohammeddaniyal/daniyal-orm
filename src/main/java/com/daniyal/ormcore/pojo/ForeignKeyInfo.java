2337ea047fc89e274c4353d6ffe5db43452634fb
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-22 11:45:30 +0530

    Added setter getter and packing

--- src/main/java/com/daniyal/ormcore/pojo/ForeignKeyInfo.java
+++ src/main/java/com/daniyal/ormcore/pojo/ForeignKeyInfo.java
@@ -1,6 +1,37 @@
+package com.daniyal.ormcore.pojo;
 public class ForeignKeyInfo
 {
-public String fkColumnName;
-public String parentTable;
-public String parentColumnName;
+private String fkColumnName;
+private  String parentTable;
+private String parentColumnName;
+public ForeignKeyInfo()
+{
+this.fkColumnName="";
+this.parentTable="";
+this.parentColumnName="";
+}
+public void setFkColumnName(java.lang.String fkColumnName)
+{
+this.fkColumnName=fkColumnName;
+}
+public java.lang.String getFkColumnName()
+{
+return this.fkColumnName;
+}
+public void setParentTable(java.lang.String parentTable)
+{
+this.parentTable=parentTable;
+}
+public java.lang.String getParentTable()
+{
+return this.parentTable;
+}
+public void setParentColumnName(java.lang.String parentColumnName)
+{
+this.parentColumnName=parentColumnName;
+}
+public java.lang.String getParentColumnName()
+{
+return this.parentColumnName;
+}
 }
