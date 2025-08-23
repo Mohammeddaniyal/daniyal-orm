e8bea8ff4d33460532da0349b7f95f7fd477386e
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-23 21:35:35 +0530

    Fixed errors and whitespace in sql

--- src/main/java/com/daniyal/ormcore/query/QueryBuilder.java
+++ src/main/java/com/daniyal/ormcore/query/QueryBuilder.java
@@ -17,29 +17,30 @@ public class QueryBuilder
 		this.fieldMetaMap=fieldMetaMap;
 		this.columnMetaMap=columnMetaMap;
 	}
-	private void processFields(FieldProcessor processor,List<String> columns,List<Object> params,StringBuilder placeholders)
+	private void processFields(FieldProcessor processor,List<String> columns,List<Object> params,StringBuilder placeholders) throws ORMException
 	{
-		try
-		{
+		
 			FieldMeta fieldMeta;
 			Field field;
-			Object rawValue;
+			Object rawValue=null;
 			Object validatedValue;
 			ColumnMetaData columnMetaData;
 			for(Map.Entry<String,FieldMeta> entry:fieldMetaMap.entrySet())
 			{
 				fieldMeta=entry.getValue();
 				field=fieldMeta.getField();
+				try
+				{
 				rawValue=field.get(this.entityInstance);
+				}catch(IllegalAccessException exception)
+				{
+					throw new ORMException("Cannot read value of field '" + field.getName() +"' in entity '" + entityInstance.getClass().getSimpleName() +"'. Ensure the field is accessible (e.g., use @Column and allow access).");		
+				}
 				validatedValue=EntityValidator.validateAndConvert(rawValue,fieldMeta,columnMetaMap.get(fieldMeta.getColumnName()));
 				processor.process(fieldMeta,validatedValue,columns,params,placeholders);
 			}
-		}catch(IllegalAccessException exception)
-		{
-			throw new ORMException("Cannot read value of field '" + field.getName() +"' in entity '" + entity.getClass().getSimpleName() +"'. Ensure the field is accessible (e.g., use @Column and allow access).");
-		}
 	}
-	public Query buildInsertQuery()
+	public Query buildInsertQuery() throws ORMException
 	{
 		List<String> columns=new ArrayList<>();
 		List<Object> params=new ArrayList<>();
@@ -53,9 +54,9 @@ public class QueryBuilder
 				ph.append(",");
 			ph.append("?");
 		};
-		processFields(insertProcessor,columns,params,placeHolder);
+		processFields(insertProcessor,columns,params,placeholders);
 		
-		String sql="INSERT INTO "+this.tableName+" ("+String.join(",",columns)+" ) VALUES ("+placeholders.toString()+ ")";
+		String sql="INSERT INTO "+this.tableName+" ("+String.join(",",columns)+") VALUES ("+placeholders.toString()+ ")";
 		return new Query(sql,params);
 	
 	}
