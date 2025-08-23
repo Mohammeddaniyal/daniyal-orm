c3433a7ed3d00f054f0aac1eb9a39d19e025f53a
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-23 17:53:40 +0530

    Feat QueryBuilder for insertQuery

---
+++ src/main/java/com/daniyal/ormcore/query/QueryBuilder.java
@@ -0,0 +1,59 @@
+package com.daniyal.ormcore.query;
+import com.daniyal.ormcore.pojo.*;
+import com.daniyal.ormcore.exceptions.*;
+public class QueryBuilder
+{
+	private final String tableName;
+	private final Map<String,FieldMeta> fieldMetaMap;
+	private final Map<String,ColumnMetaData> columnMetaMap;
+	private final Object entityInstance;
+	public QueryBuilder(Object entityInstance,String tableName,Map<String,FieldMeta> fieldMetaMap,Map<String,ColumnMetaData> columnMetaMap)
+	{
+		this.entityInstance=entityInstance;
+		this.tableName=tableName;
+		this.fieldMetaMap=fieldMetaMap;
+		this.columnMetaMap=columnMetaMap;
+	}
+	private void processFields(FieldProcessor processor,List<String> columns,List<Object> params,StringBuilder placeholders)
+	{
+		try
+		{
+			FieldMeta fieldMeta;
+			Field field;
+			Object rawValue;
+			Object validatedValue;
+			ColumnMetaData columnMetaData;
+			for(Map.Entry<String,FieldMeta> entry:fieldMetaMap.entrySet())
+			{
+				fieldMeta=entry.getValue();
+				field=fieldMeta.getField();
+				rawValue=field.get(this.entityInstance);
+				validatedValue=EntityValidator.validateAndConvert(rawValue,fieldMeta,columnMetaMap.get(fieldMeta.getColumnName()));
+				processor.process(fieldMeta,validatedValue,columns,params,placeholders);
+			}
+		}catch(IllegalAccessException exception)
+		{
+			throw new ORMException("Cannot read value of field '" + field.getName() +"' in entity '" + entity.getClass().getSimpleName() +"'. Ensure the field is accessible (e.g., use @Column and allow access).");
+		}
+	}
+	public Query buildInsertQuery()
+	{
+		List<String> columns=new ArrayList<>();
+		List<Object> params=new ArrayList<>();
+		StringBuilder placeholders=new StringBuilder();
+		FieldProcessor insertProcessor=(fieldMeta,validatedValue,cols,paramList,ph)->{
+			if(fieldMeta.getIsAutoIncrement()) 
+				return;
+			columns.add(fieldMeta.getColumnName());
+			params.add(validatedValue);
+			if(ph.length()>0) 
+				ph.append(",");
+			ph.append("?");
+		};
+		processFields(insertProcessor,columns,params,placeHolder);
+		
+		String sql="INSERT INTO "+this.tableName+" ("+String.join(",",columns)+" ) VALUES ("+placeholders.toString()+ ")";
+		return new Query(sql,params);
+	
+	}
+}
