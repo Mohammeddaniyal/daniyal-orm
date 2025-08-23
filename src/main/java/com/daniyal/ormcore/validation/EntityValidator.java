7bd88d9c38e414e2265a7f25b32499461e5f47ff
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-23 11:56:45 +0530

    Created EntityValidator

---
+++ src/main/java/com/daniyal/ormcore/validation/EntityValidator.java
@@ -0,0 +1,10 @@
+package com.daniyal.ormcore.validation;
+import com.daniyal.ormcore.exceptions.*;
+import com.daniyal.ormcore.pojo.*;
+public class EntityValidator
+{
+	public static Object validateAndConvert(Object value,FieldMeta fieldMeta,ColumnMetaData columnMetaData)
+	{
+		
+	}
+}
