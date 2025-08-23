0338ea9030bcbec42d72d3e5d46e35c1c499a5a9
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-23 21:21:03 +0530

    Added import for FieldProcessor

--- src/main/java/com/daniyal/ormcore/query/FieldProcessor.java
+++ src/main/java/com/daniyal/ormcore/query/FieldProcessor.java
@@ -1,9 +1,10 @@
 package com.daniyal.ormcore.query;
 import com.daniyal.ormcore.pojo.*;
 import com.daniyal.ormcore.exceptions.*;
+import java.util.*;
 
 @FunctionalInterface
 interface FieldProcessor
 {
-	void process(FieldMeta fieldMeta,List<String> columns,List<Object> params,StringBuilder placeHolder) throws ORMException;
+	void process(FieldMeta fieldMeta,Object validatedValue,List<String> columns,List<Object> params,StringBuilder placeHolder) throws ORMException;
 }
