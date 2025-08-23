8ca7982e6c5edb2593c609d44c915668fc7d7f8f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-23 21:21:28 +0530

    Added import for QueryBuilder

--- src/main/java/com/daniyal/ormcore/query/QueryBuilder.java
+++ src/main/java/com/daniyal/ormcore/query/QueryBuilder.java
@@ -1,6 +1,7 @@
 package com.daniyal.ormcore.query;
 import com.daniyal.ormcore.pojo.*;
 import com.daniyal.ormcore.exceptions.*;
+import java.util.*;
 public class QueryBuilder
 {
 	private final String tableName;
