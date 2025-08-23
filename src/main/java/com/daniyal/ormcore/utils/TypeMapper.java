045a563adf96f2ed9ca18bebd6b6ed49f091b412
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-23 10:32:41 +0530

    Feat added isCompatible method

--- src/main/java/com/daniyal/ormcore/utils/TypeMapper.java
+++ src/main/java/com/daniyal/ormcore/utils/TypeMapper.java
@@ -1,7 +1,7 @@
 package com.daniyal.ormcore.utils;
-class TypeMapper
+public class TypeMapper
 {
-private static String  getFieldType(String type,int size)
+public static String  getFieldType(String type,int size)
 {
 if(type.equalsIgnoreCase("INT")) return "int";
 if(type.equalsIgnoreCase("BIGINT")) return "long";
@@ -18,5 +18,44 @@ if(type.equalsIgnoreCase("DATE")) return "Date";
 //if(type.equalsIgnoreCase("TIME")) return "Time";
 //if(type.equalsIgnoreCase("TIMESTAMP")  || type.equalsIgnoreCase("DATETIME")) return "TimeStamp";
 return "Object";
+}
+public static boolean isCompatible(String entityFieldType,String tableFieldType,int size)
+{
+switch(tableFieldType.toUpperCase())
+{
+	case "INT":
+	return entityFieldType.equals("int") || entityFieldType.equals("java.lang.Integer");
+	case "BIGINT"
+	return entityFieldType.equals("long") || entityFieldType.equals("java.lang.Long");
+	case "DOUBLE"
+	return entityFieldType.equals("double") || entityFieldType.equals("java.lang.Double");
+	case "FLOAT":
+	return entityFieldType.equals("float") || entityFieldType.equals("java.lang.Float");
+	case "BOOLEAN":
+	caee "BIT":
+	return entityFieldType.equals("boolean") || entityFieldType.equals("java.lang.Boolean");
+	
+	case "VARCHAR":
+	case "TEXT":
+	return entityFieldType.equals("java.lang.String");
+	case "CHAR":
+	if(size==1)
+	{
+		return entityFieldType.equals("char") || entityFieldType.equals("java.lang.Character");
+	}
+	else
+	{
+		return entityFieldType.equals("java.lang.String");
+	}
+	case "DATE":
+	return entityFieldType.equals("java.util.Date") || entityFieldType.equals("java.sql.Date");
+	case "TIMESTAMP":
+	return entityFieldType.equals("java.sql.TimeStamp") || entityFieldType.equals("java.util.Date");
+	case "DECIMAL":
+	case "NUMERIC":
+	return entityFieldType.equals("double") || entityFieldType.equals("java.lang.Double") || entityFieldType.equals("java.math.BigDecimal");
+	default:
+	return false;
+}
 }
 }
