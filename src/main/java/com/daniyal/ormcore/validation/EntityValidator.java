27397c60bab95cad37ad6be1de88392f5fdd939f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-23 12:44:24 +0530

    Handled validation for BigDecimla

--- src/main/java/com/daniyal/ormcore/validation/EntityValidator.java
+++ src/main/java/com/daniyal/ormcore/validation/EntityValidator.java
@@ -1,10 +1,69 @@
 package com.daniyal.ormcore.validation;
 import com.daniyal.ormcore.exceptions.*;
 import com.daniyal.ormcore.pojo.*;
+import java.util.*;
+import java.sql.*;
+import java.math.*;
+
 public class EntityValidator
 {
-	public static Object validateAndConvert(Object value,FieldMeta fieldMeta,ColumnMetaData columnMetaData)
+	public static Object validateAndConvert(Object value,FieldMeta fieldMeta,ColumnMetaData columnMetaData) throws ORMException
 	{
-		
+		if(value==null)
+		{
+			if(!columnMetaData.getIsNull())
+			{
+				throw new ORMException("Field '" + fieldMeta.getField().getName() + "' cannot be null.");
+			}
+			return null;
+		}
+		else
+		{
+			if(value instanceof String)
+			{
+				int maxSize=columnMetaData.getSize();
+				String str=(String)value;
+				if(str.length()>maxSize)
+				{
+					throw new ORMException("Value for field '" + fieldMeta.getField().getName() + "' exceeds max length of " + maxSize);
+				}
+			}else if(value instanceof Character)
+			{
+					value=String.valueOf(value);
+			}else if(value instanceof java.util.Date)
+			{
+				java.util.Date utilDate=(java.util.Date)value;
+				String sqlType=columnMetaData.getType();
+				if("DATE".equals(sqlType))
+				{
+					return new java.sql.Date(utilDate.getTime());
+				}else if("TIMESTAMP",equals(sqlType) || "DATETIME".equals(sqlType))
+				{
+					return new java.sql.Timestamp(utilDate.getTime());
+				}else{
+					throw new ORMException("Field '" + fieldMeta.getField().getName() +"' is Date but column type is '" + sqlType + "'");
+				}
+			}else if(value instanceof BigDecimal)
+			{
+				BigDecimal normalized=value.stripTrailingZeros();
+				
+				int precision=normalized.precision();
+				int valueScale=normalized.scale();
+				
+				int maxDigits=	columnMetaData.getSize();
+				int scale= columnMetaData.getScale();
+				if (valuePrecision > precision) 
+				{
+            throw new ORMException("BigDecimal value for field '" + fieldName +
+                "' exceeds the maximum precision allowed (" + precision + "). Actual: " + valuePrecision);
+				}
+			if (valueScale > scale) 
+				{
+            throw new ORMException("BigDecimal value for field '" + fieldName +
+                "' exceeds the maximum scale allowed (" + scale + "). Actual: " + valueScale);
+				}
+			}
+			return value;
+		}
 	}
 }
