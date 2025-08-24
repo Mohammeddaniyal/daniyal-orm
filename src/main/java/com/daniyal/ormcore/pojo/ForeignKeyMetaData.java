7e680e7ca454948498069b56c1e471fdac2db64b
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-24 16:29:57 +0530

    Refacoted file name

---
+++ src/main/java/com/daniyal/ormcore/pojo/ForeignKeyMetaData.java
@@ -0,0 +1,37 @@
+package com.daniyal.ormcore.pojo;
+public class ForeignKeyInfo
+{
+private String fkColumn;
+private  String pkTable;
+private String pkColumn;
+public ForeignKeyInfo()
+{
+this.fkColumn="";
+this.pkTable="";
+this.fkColumn="";
+}
+public void setFKColumn(java.lang.String fkColumn)
+{
+this.fkColumn=fkColumn;
+}
+public java.lang.String getFKColumn()
+{
+return this.fkColumn;
+}
+public void setPKTable(java.lang.String pkTable)
+{
+this.pkTable=pkTable;
+}
+public java.lang.String getPKTable()
+{
+return this.pkTable;
+}
+public void setPKColumn(java.lang.String pkColumn)
+{
+this.pkColumn=pkColumn;
+}
+public java.lang.String getPKColumn()
+{
+return this.pkColumn;
+}
+}
