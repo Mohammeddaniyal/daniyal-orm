6f0ec684dac19f3412a9f1e226b73f93255f5aad
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-25 22:49:21 +0530

    Added generic handling

--- src/main/java/com/daniyal/ormcore/query/Condition.java
+++ src/main/java/com/daniyal/ormcore/query/Condition.java
@@ -1,40 +1,40 @@
 package com.daniyal.ormcore.query;
-public class Condition
+public class Condition<T>
 {
-	private final QueryBuilder parent;
+	private final QueryBuilder<T> parent;
 	private final String column;
 	
-	Condition(QueryBuilder parent,String column)
+	Condition(QueryBuilder<T> parent,String column)
 	{
 		this.parent=parent;
 		this.column=column;
 	}
-	public QueryBuilder eq(Object value)
+	public QueryBuilder<T> eq(Object value)
 	{
 		parent.addCondition(column+" =?",value);
 		return parent;
 	}
-	public QueryBuilder gt(Object value)
+	public QueryBuilder<T> gt(Object value)
 	{
 		parent.addCondition(column+" > ?",value);
 		return parent;
 	}
-	public QueryBuilder lt(Object value)
+	public QueryBuilder<T> lt(Object value)
 	{
 		parent.addCondition(column+" < ?",value);
 		return parent;
 	}
-	public QueryBuilder ge(Object value)
+	public QueryBuilder<T> ge(Object value)
 	{
 		parent.addCondition(column+" >=?",value);
 		return parent;
 	}
-	public QueryBuilder le(Object value)
+	public QueryBuilder<T> le(Object value)
 	{
 		parent.addCondition(column+" <=?",value);
 		return parent;
 	}
-	public QueryBuilder like(Object value)
+	public QueryBuilder<T> like(Object value)
 	{
 		parent.addCondition(column+" LIKE ?",value);
 		return parent;
