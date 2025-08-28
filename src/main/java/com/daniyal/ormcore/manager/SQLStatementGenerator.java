e007c35f42abbfa5243c0e4bd6e88c0860b0d1f3
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-28 12:11:49 +0530

    IMplementing SQLStatementGenerator

---
+++ src/main/java/com/daniyal/ormcore/manager/SQLStatementGenerator.java
@@ -0,0 +1,9 @@
+package com.daniyal.ormcore.manager;
+import com.daniyal.ormcore.pojo.*;
+class SQLStatementGenerator
+{
+	public static Map<Class,SQLStatement> generateSQLStatementMap()
+	{
+		
+	}
+}
