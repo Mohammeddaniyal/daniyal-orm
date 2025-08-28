49b9bb05c8a63f77d57ae4b4bc08c39ed1f3bb33
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-28 16:07:50 +0530

    Feat SQLStatementGenerator

--- src/main/java/com/daniyal/ormcore/manager/SQLStatementGenerator.java
+++ src/main/java/com/daniyal/ormcore/manager/SQLStatementGenerator.java
@@ -1,22 +1,37 @@
 package com.daniyal.ormcore.manager;
 import com.daniyal.ormcore.pojo.*;
+import com.daniyal.ormcore.exceptions.*;
+import com.daniyal.ormcore.query.QueryBuilder;
 import java.util.*;
 class SQLStatementGenerator
 {
-	public static Map<Class,SQLStatement> generateSQLStatementMap(Map<Class,EntityMetaData> entityMetaDataMap)
+	public static Map<Class,SQLStatement> generateSQLStatementMap(Map<Class,EntityMetaData> entityMetaDataMap) throws ORMException
 	{
 		Map<Class,SQLStatement> sqlStatementMap=new HashMap<>();
 		EntityMetaData entityMetaData;
 		Class entityClass;
-		FieldMetaDataMap<String,FieldMetaData> fieldMetaDataMap;
+		Map<String,FieldMetaData> fieldMetaDataMap;
 		QueryBuilder queryBuilder;
 		SQLStatement sqlStatement;
 		String insertSQL;
-		for(Map.Entry<Class,EntityMetaData> entityMetaDataEntry:entityMetaDataMap)
+		String updateSQL;
+		String deleteSQL;
+		String selectAllSQL;
+		FieldMetaData primaryKeyFieldMetaData;
+		String primaryKeyColumn;
+		for(Map.Entry<Class,EntityMetaData> entityMetaDataEntry:entityMetaDataMap.entrySet())
 		{
 			entityMetaData=entityMetaDataEntry.getValue();
 			entityClass=entityMetaDataEntry.getKey();
+			primaryKeyFieldMetaData=entityMetaData.getPrimaryKeyFieldMetaData();
+			primaryKeyColumn=primaryKeyFieldMetaData.getColumnName();
 			queryBuilder=new QueryBuilder(entityMetaData.getTableName(),entityMetaData.getFieldMetaDataMap());
+			insertSQL=queryBuilder.buildInsertSQL();
+			updateSQL=queryBuilder.buildUpdateSQL(primaryKeyColumn);
+			deleteSQL=queryBuilder.buildDeleteSQL(primaryKeyColumn);
+			selectAllSQL=queryBuilder.buildSelectAllSQL();
+			sqlStatement=new SQLStatement(insertSQL,updateSQL,deleteSQL,selectAllSQL);
+			sqlStatementMap.put(entityClass,sqlStatement);
 		}
 		return sqlStatementMap;
 	}
